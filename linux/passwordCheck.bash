c3486a68e5fd61dcccd9ac6ba16326b88454781f
schoolyo <112569021+schoolyo@users.noreply.github.com>
2022-10-04 10:30:20 -0500

    Create passwordCheck.bash

---
+++ linux/passwordCheck.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This program will check if any users don't have a password, and prompt to create a new one if they don't
+
+checkEmpty() {
+# List all users with no password
+read -p "Check if any users have no password... [ENTER]"
+echo
+grep 
