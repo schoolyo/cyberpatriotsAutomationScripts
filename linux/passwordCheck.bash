0055d7d5e0963ab407883205ebe080ad3bdbab25
schoolyo <112569021+schoolyo@users.noreply.github.com>
2022-10-05 10:04:49 -0500

    Update passwordCheck.bash

--- linux/passwordCheck.bash
+++ linux/passwordCheck.bash
@@ -9,3 +9,18 @@ echo
 awk -F':' '{if ($2 == "*" || $2 == "!") print $1}' /etc/shadow 
 echo
 }
+
+setNew() {
+# Set new passwords for users that have none
+
+}
+
+if [ "$(id -u)" != "0" ]; then
+
+echo "You are not running passwordCheck.bash as root."
+echo "Run as 'sudo bash passwordCheck.bash'"
+exit
+else
+checkEmpty
+setNew
+fi
