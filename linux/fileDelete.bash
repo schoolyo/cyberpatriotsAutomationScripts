3e3718400abc6779fb93ba45318502cfe33fd422
schoolyo <112569021+schoolyo@users.noreply.github.com>
2022-10-06 21:41:26 -0500

    Update fileDelete.bash

--- linux/fileDelete.bash
+++ linux/fileDelete.bash
@@ -1,7 +1,8 @@
 # This program will use the output files from fileFinder.bash and use the file locations as arguments to delete the files
+# remember to call this script with the input file name as a parameter
 delete() {
 # delete the files
-while read -r i ; do rm $i ; done < filename
+while read -r i ; do rm $i ; done < $1
 }
 # Make sure to run as root
 if [ "$(id -u)" != "0" ]; then
