66fa778bbd491e5331b8b85b139da58a5c0115c6
schoolyo <112569021+schoolyo@users.noreply.github.com>
2022-10-06 21:58:17 -0500

    Update passwordCheck.bash

--- linux/passwordCheck.bash
+++ linux/passwordCheck.bash
@@ -15,18 +15,33 @@ getRealUsers() {
 l=$(grep "^UID_MIN" /etc/login.defs)
 l1=$(grep "^UID_MAX" /etc/login.defs)
 userLst=`awk -F':' -v "min=${l##UID_MIN}" -v "max=${l1##UID_MAX}" '{ if ( $3 >= min && $3 <= max ) print $0}' /etc/passwd | cut -d: -f1`
-
+# still need to figure format of output, and how to parse if needed
 }
 
 setNew() {
 # Set new passwords for users that have none
 read -p "Set new passwords for any users that have none"
+echo
+while :
+do
+echo "Enter the username of the user you want to create a new password for. Type 0 to exit."
+read a 
+if [ $a -eq 0 ]; then
+break
+else
+# need to figure out how to read the list
+echo "New password for $a has been created"
+read -p "[ENTER] to continue..."
+
+fi
+
+done
 }
 
 if [ "$(id -u)" != "0" ]; then
 
 echo "You are not running passwordCheck.bash as root."
-echo "Run as 'sudo bash passwordCheck.bash'"
+echo "Run as sudo bash passwordCheck.bash"
 exit
 else
 checkEmpty
