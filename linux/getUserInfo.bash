3e1ba7fc56c2d1a3a242dea6e4600d66b0b7afbe
schoolyo <112569021+schoolyo@users.noreply.github.com>
2022-10-07 10:30:21 -0500

    Update getUserInfo.bash

--- linux/getUserInfo.bash
+++ linux/getUserInfo.bash
@@ -1,16 +1,17 @@
+#!/bin/bash
 # This file will list relevant data about a user given the username as input. 
 # Useful for forensics questions that ask about users
 
 getInfo() {
-# get specified users info
-if [[ ! -z $1 ]]; then
-a=$1
-else
-echo "Please enter the username you want info about"
-read a
-fi
-
-# Display the info
-UID=`id $a`
-grep "^${a}" /etc/shadow | cut -d: 
+uid=`id $1`
+passwdStat=`grep "^${1}" /etc/shadow | cut -d: -f2`
+echo "The UID of $1 is $uid"
+echo "$1 has $passwdStat password status"
 }
+
+if [ "$(id -u)" != ]; then
+echo "You are not running getUserInfo.bash as root. Please do."
+exit
+else
+getInfo $1
+fi
