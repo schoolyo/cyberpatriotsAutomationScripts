87b3ed971b45afa4d385be424eb863e8352b3a25
schoolyo <112569021+schoolyo@users.noreply.github.com>
2022-10-19 09:59:10 -0500

    Update passwordCheck.bash

--- linux/passwordCheck.bash
+++ linux/passwordCheck.bash
@@ -2,20 +2,25 @@
 
 # This program will check if any users don't have a password, and prompt to create a new one if they don't
 
-checkEmpty() {
-# List all users with no password
-read -p "Check if any users have no password... [ENTER]"
-echo
-awk -F':' '{if ($2 == "*" || $2 == "!") print $1}' /etc/shadow 
-echo
-}
-
-getRealUsers() {
+getRealPasswordless() {
 # Get the list of actual users and cross reference with users that have no password to get rid of system users
+read -p "Check if any users have no password... [ENTER]"
+
+# get min and max uid values for users
 l=$(grep "^UID_MIN" /etc/login.defs)
 l1=$(grep "^UID_MAX" /etc/login.defs)
+# get strings of usernames that have no passwords and are real users
 userLst=`awk -F':' -v "min=${l##UID_MIN}" -v "max=${l1##UID_MAX}" '{ if ( $3 >= min && $3 <= max ) print $0}' /etc/passwd | cut -d: -f1`
-# still need to figure format of output, and how to parse if needed
+emptyLst=`awk -F':' '{if ( $2 == "*" || $2 == "!" ) print $1}' /etc/shadow`
+
+# display real passwordless users
+for user in $userLst; do
+  for none in $emptyLst; do
+    if [ $user == $none ]; then
+      echo $user
+    fi
+  done
+done
 }
 
 setNew() {
@@ -44,6 +49,6 @@ echo "You are not running passwordCheck.bash as root."
 echo "Run as sudo bash passwordCheck.bash"
 exit
 else
-checkEmpty
+getRealPasswordless
 setNew
 fi
