2287bea3a48bea55f4dbf604535b583e44edb1f7
schoolyo <112569021+schoolyo@users.noreply.github.com>
2022-10-24 11:10:29 -0500

    Update fileFinder.sh

--- linux/fileFinder.sh
+++ linux/fileFinder.sh
@@ -7,23 +7,23 @@ mkdir ./out
 
 # Audio
 
-find /home -name "*.midi" -type f | tee ./out/audio.out
-find /home -name "*.mid" -type f | tee ./out/audio.out
-find /home -name "*.mp3" -type f | tee ./out/audio.out
-find /home -name "*.flac" -type f | tee ./out/audio.out
+find /home -name "*.midi" -type f | tee -a ./out/audio.out
+find /home -name "*.mid" -type f | tee -a ./out/audio.out
+find /home -name "*.mp3" -type f | tee -a ./out/audio.out
+find /home -name "*.flac" -type f | tee -a ./out/audio.out
 
 # Images
 
-find /home -name "*.jpg" -type f | tee ./out/image.out
-find /home -name "*.png" -type f | tee ./out/image.out
-find /home -name "*.jpeg" -type f | tee ./out/image.out
-find /home -name "*.gif" -type f | tee ./out/image.out
+find /home -name "*.jpg" -type f | tee -a ./out/image.out
+find /home -name "*.png" -type f | tee -a ./out/image.out
+find /home -name "*.jpeg" -type f | tee -a ./out/image.out
+find /home -name "*.gif" -type f | tee -a ./out/image.out
 
 # Video
 
-find /home -name "*.avi" -type f | tee ./out/video.out
-find /home -name "*.mp4" -type f | tee ./out/video.out
-find /home -name "*.wmv" -type f | tee ./out/video.out
-find /home -name "*.mpg" -type f | tee ./out/video.out
-find /home -name "*.MPG" -type f | tee ./out/video.out
-find /home -name ".*mpeg" -type f | tee ./out/video.out
+find /home -name "*.avi" -type f | tee -a ./out/video.out
+find /home -name "*.mp4" -type f | tee -a ./out/video.out
+find /home -name "*.wmv" -type f | tee -a ./out/video.out
+find /home -name "*.mpg" -type f | tee -a ./out/video.out
+find /home -name "*.MPG" -type f | tee -a ./out/video.out
+find /home -name ".*mpeg" -type f | tee -a ./out/video.out
