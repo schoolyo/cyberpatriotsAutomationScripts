e36e2e7211cc68fff6d86e48114d3439469fcec4
schoolyo <112569021+schoolyo@users.noreply.github.com>
2023-09-12 08:47:44 -0500

    Update getUserInfo.sh with indentations

--- linux/getUserInfo.sh
+++ linux/getUserInfo.sh
@@ -3,18 +3,18 @@
 # Useful for forensics questions that ask about users
 
 getInfo() {
-uid=`id $1`
-passwdStat=`grep "^${1}" /etc/shadow | cut -d: -f2`
-echo "The ID's of $1 is $uid"
-echo "$1 has $passwdStat password status"
+  uid=`id $1`
+  passwdStat=`grep "^${1}" /etc/shadow | cut -d: -f2`
+  echo "The ID's of $1 is $uid"
+  echo "$1 has $passwdStat password status"
 }
 
 if [ "$(id -u)" != "0" ]; then
-echo "You are not running getUserInfo.bash as root. Please do."
-exit
+  echo "You are not running getUserInfo.bash as root. Please do."
+  exit
 elif [ $# -eq 0 ]; then
-echo "No argument provided. Please provide the username you want info about as a parameter when calling the script"
-exit
+  echo "No argument provided. Please provide the username you want info about as a parameter when calling the script"
+  exit
 else
-getInfo $1
+  getInfo $1
 fi
