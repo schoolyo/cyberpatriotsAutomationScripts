0741d994f911b90276ad93b2262194bd6d5c18b5
schoolyo <112569021+schoolyo@users.noreply.github.com>
2023-10-13 15:36:03 -0500

    make searching case-insensitive

--- linux/fileFinder.sh
+++ linux/fileFinder.sh
@@ -7,23 +7,23 @@ mkdir ./out
 
 # Audio
 
-find /home -name "*.midi" -type f | tee -a ./out/audio.out
-find /home -name "*.mid" -type f | tee -a ./out/audio.out
-find /home -name "*.mp3" -type f | tee -a ./out/audio.out
-find /home -name "*.flac" -type f | tee -a ./out/audio.out
+find /home -iname "*.midi" -type f | tee -a ./out/audio.out
+find /home -iname "*.mid" -type f | tee -a ./out/audio.out
+find /home -iname "*.mp3" -type f | tee -a ./out/audio.out
+find /home -iname "*.flac" -type f | tee -a ./out/audio.out
 
 # Images
 
-find /home -name "*.jpg" -type f | tee -a ./out/image.out
-find /home -name "*.png" -type f | tee -a ./out/image.out
-find /home -name "*.jpeg" -type f | tee -a ./out/image.out
-find /home -name "*.gif" -type f | tee -a ./out/image.out
+find /home -iname "*.jpg" -type f | tee -a ./out/image.out
+find /home -iname "*.png" -type f | tee -a ./out/image.out
+find /home -iname "*.jpeg" -type f | tee -a ./out/image.out
+find /home -iname "*.gif" -type f | tee -a ./out/image.out
 
 # Video
 
-find /home -name "*.avi" -type f | tee -a ./out/video.out
-find /home -name "*.mp4" -type f | tee -a ./out/video.out
-find /home -name "*.wmv" -type f | tee -a ./out/video.out
-find /home -name "*.mpg" -type f | tee -a ./out/video.out
-find /home -name "*.MPG" -type f | tee -a ./out/video.out
-find /home -name ".*mpeg" -type f | tee -a ./out/video.out
+find /home -iname "*.avi" -type f | tee -a ./out/video.out
+find /home -iname "*.mp4" -type f | tee -a ./out/video.out
+find /home -iname "*.wmv" -type f | tee -a ./out/video.out
+find /home -iname "*.mpg" -type f | tee -a ./out/video.out
+find /home -iname "*.mpeg" -type f | tee -a ./out/video.out
+find /home -iname "*.mov" -type f | tee -a ./out/video.out
